1b18880ff773764af25d9ba93b85b12c7d8a7ad6
Babaylov7 <babaylov-ayu@mail.ru>
2023-12-15 14:52:08 +0500

    2

--- app/src/main/java/com/example/sprint_18_2/MainActivity.kt
+++ app/src/main/java/com/example/sprint_18_2/MainActivity.kt
@@ -7,16 +7,49 @@ import com.example.sprint_18_2.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding? = null
-
+    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if(savedInstanceState == null){
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_container_view, CitiesFragment())
                 .commit()
         }
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.replaceCountryFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, CountriesFragment())
+                .setReorderingAllowed(true)
+                .commit()
+        }
+
+        binding.replaceBackStackCountryFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, CountriesFragment())
+                .addToBackStack("countries")
+                .setReorderingAllowed(true)
+                .commit()
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
